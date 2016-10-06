ac337e623f744864f3e80da003645062b51b9042
dianaantunes <diana.antunes@tecnico.ulisboa.pt>
2016-10-06 23:28:25 +0100

    working tcp
    needs some cleaning

--- user.c
+++ user.c
@@ -10,8 +10,11 @@
 #include <netdb.h>
 #include <signal.h>
 
+#define False 0
+#define True 1
 
 int connectTRS(char* message);
+int countWords(char* s);
 
 int main(int argc, char** argv) {
 
@@ -22,7 +25,6 @@ int main(int argc, char** argv) {
     char TCSname[32] = "localhost";
     char instruction[32]; 
     char languages[32][99];
-    char* token;
     int TCSport = 58000;
 
     // Argument reading
@@ -120,31 +122,29 @@ int main(int argc, char** argv) {
 
         scanf("%s", instruction);
     }
-
-
     close(fdUDP);
     /*
     hostptr = gethostbyaddr((char*)&addr.sin_addr, sizeof(struct in_addr), AF_INET);
     if (hostptr == NULL) printf("sent by [%s:%hu]\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
     else printf("sent by [%s:%hu]", hostptr -> h_name, ntohs(addr.sin_port));
     */
-
     exit(0);
-
 }
+
 int connectTRS(char* message){
     // message is the info necessary to connect to TRS, given by TCS
     void (*old_handler)(int); //interrupt hanlder
 
     if ((old_handler = signal(SIGPIPE, SIG_IGN)) == SIG_ERR) exit(1); // Error
 
-    int fdTCP, n, nbytes, nleft, nwritten, nread;
+    int fdTCP, n, nbytes, nleft, nwritten, nread, nWords, i=0;
     struct sockaddr_in addr;
-    struct hostent *hostptr;
-    char *ptr, buffer[128];
+    char *ptr;
     char TRSname[32] = "localhost";
-    char instruction[320]; 
-    char type[2];
+    char request[8] = "TRQ ";
+    char buffer[320]; 
+    char words_str[320];
+    char type[8];
     char* token;
     int TRSport = 59000;
 
@@ -166,58 +166,58 @@ int connectTRS(char* message){
 
     n = connect(fdTCP, (struct sockaddr*)&addr, sizeof(addr)); // connect to TRS
     if (n == -1) perror("Failed to connect");
-    
-    printf("Open TCP\n");
-    
-    memset(instruction, "\0", 320); // initializing the instruction with /0
-    strcpy(instruction, "TRQ"); 
-    fgets(&instruction[3], 315, stdin); // getting the info given by the user to send to TRS
+
+    printf("%s %d\n", TRSname, TRSport); // print TRS info
+
+    memset(buffer, (int)'\0', 320); // initializing the buffer with /0
+    strcpy(buffer, request); 
+
+    fgets(words_str, 319, stdin); // getting the info given by the user to send to TRS
     // fgets keeps the spaces !!!
 
-    memcpy(type, &instruction[4], 1); // copy the type from instruction
-    //strcat(type, "\0");
-
-    printf("%s\n", type);
-    printf("%s\n", instruction);
-    printf("%d\n", strlen(instruction));
+    n = sscanf(words_str, "%s", type);
+    
     // the type has to be text(t) or file (f)
     if (!strcmp(type, "f")){
 
 
     } 
+
     else if (!strcmp(type, "t")){
+        nWords = countWords(words_str) - 1; // remove type from count
         
+        strcat(buffer, type);
+        strcat(buffer, " ");
+        
+        sprintf(type, "%d", nWords);
+        strcat(buffer, type);
+        
+        strcat(buffer, &words_str[2]);
     }
+
     else{
-        printf("Usage: request n t N W1 W2 ... Wn OR request n f filename\n");
-        //exit(1);
+        printf("Usage: request n t W1 W2 ... Wn OR request n f filename\n");
+        return 1;
     }
     
-    /*
-    ptr = instruction;
-    nbytes = strlen(instruction);
+
+    ptr = buffer;
+    nbytes = strlen(buffer);
     
     nleft = nbytes;
    
     while(nbytes > 0) {
-        nwritten = write(fdTCP, instruction, nbytes);
+        nwritten = write(fdTCP, ptr, nleft);
         if (nwritten <= 0) perror("Falha a enviar mensagem");
         nbytes -= nwritten;
         ptr += nwritten;
     }
-    
+
+    memset(buffer, (int)'\0', 320); // initializing the buffer with /0
 
     
-    write(1, "echo: ", 6); //stdout
-    write(1, instruction, nread);*/
-    n = write(fdTCP,instruction,strlen(instruction));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(instruction,256);
-
-    /*
-    nleft = nbytes;
-    ptr = &instruction[0];
+    nleft = 320;
+    ptr = buffer;
     while(nleft > 0) {
         nread = read(fdTCP, ptr, nleft);
         if (nread == -1) perror("Falha a ler mensagem");
@@ -226,14 +226,39 @@ int connectTRS(char* message){
         ptr += nread;
     }
 
-    nread = nbytes - nleft;
-    */
-    n = read(fdTCP,instruction,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",instruction);
+    strcpy(words_str, buffer);
+
+    memset(buffer, (int)'\0', 320); // initializing the buffer with /0
+
+    token = strtok(words_str, " "); // use this to check for error
+    token = strtok(NULL, " "); // ditch the type
+    token = strtok(NULL, " "); // ditch the numWords
+
+    while (i < nWords){
+                strcat(buffer, strtok(NULL, " "));
+                strcat(buffer, " ");
+                i++;
+            }
+
+    printf("%s: %s \n", TRSname, buffer);
 
     close(fdTCP);
-    printf("Closed TCP\n");
+    return 0;
 }
 
+int countWords(char* s){
+ int count = 0, i;
+ int foundLetter = False;
+ for (i = 0;i<strlen(s);i++)
+ {
+  if (s[i] == ' ')
+      foundLetter = False;
+  else 
+  {    
+      if (foundLetter == False)
+          count++;
+      foundLetter = True;
+  }
+ }
+ return count;
+}
